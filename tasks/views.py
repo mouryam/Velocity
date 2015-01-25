ba1259e385c94f0acf223aa35bedfa1f44143797
Shureed <skabir1717@gmail.com>
2015-01-25 15:41:18 -0500

    created task view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from tasks.models import Task
+from django.views.generic import ListView
 
 # Create your views here.
+
+class ListTaskView(ListView):
+    model = Task
